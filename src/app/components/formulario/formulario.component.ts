54be4d5f4c0ade47729fcb48be7e52fdbd9d399c
Soft-listig <soft.listig@gmail.com>
2018-06-21 00:09:02 -0500

    Formulario terminado

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -50,8 +50,72 @@ export class FormularioComponent implements OnInit {
     investigacion_desarrollo: [''],
     investigacion_enseñanza: [''],
     infraestructura: [''],
-    residencia: ['']
-
+    residencia: [''],
+    iii1: [''],
+    iii1_esp_select: [''],
+    iii1_esp_input: [''],
+    iii1_otra: [''],
+    iii2: [''],
+    iii3: [''],
+    iii4: [''],
+    iii4_o: [''],
+    iii5: [''],
+    iii5_o: [''],
+    iii6_hablar: [''],
+    iii6_escribir: [''],
+    iii6_leer: [''],
+    iii6_escuchar: [''],
+    iii6: [''],
+    iii7: [''],
+    iii7_ingreso: [''],
+    iii8: [''],
+    iii9: [''],
+    iii10: [''],
+    iii10_o: [''],
+    iii11: [''],
+    iii12: [''],
+    iii12_giro: [''],
+    iii12_razon: [''],
+    iii12_calle: [''],
+    iii12_numero: [''],
+    iii12_colonia: [''],
+    iii12_cp: [''],
+    iii12_ciudad: [''],
+    iii12_municipio: [''],
+    iii12_estado: [''],
+    iii12_tel: [''],
+    iii12_fax: [''],
+    iii12_email: [''],
+    iii12_web: [''],
+    iii12_nombre: [''],
+    iii12_puesto: [''],
+    iii13_primario: [''],
+    iii13_secundario: [''],
+    iii13_terciario: [''],
+    iii14: [''],
+    iv1: [''],
+    iv2: [''],
+    iv3: [''],
+    iv4_1: [''],
+    iv4_2: [''],
+    iv4_3: [''],
+    iv4_4: [''],
+    iv4_5: [''],
+    iv4_6: [''],
+    iv4_7: [''],
+    iv4_8: [''],
+    iv4_9: [''],
+    iv4_10: [''],
+    v1_a: [''],
+    v1_b: [''],
+    v2_a: [''],
+    v2_b: [''],
+    vi1: [''],
+    vi1_b: [''],
+    vi2: [''],
+    vi2_b: [''],
+    vi3: [''],
+    vii: ['']
   });
 }
   public ngOnInit() {
@@ -59,9 +123,8 @@ export class FormularioComponent implements OnInit {
 
   main_calcular() {
     this.formulario = this.perfil.value;
-    console.log(this.formulario);
-    this.afDB.list(`/Posts/`).push(this.formulario).then(()=>{
-    console.log(this.formulario);
+    const key = this.formulario.matricula;
+    this.afDB.object(`/Posts/{${key}}`).update(this.formulario).then(() => {
     this.flag = true;
 
   });
